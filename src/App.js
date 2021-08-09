77c575e317158afe68aea1a0466b744bf54f033c
MasakiHirabayashi <get_dreams_masax_in_the_mirror@yahoo.co.jp>
2021-08-09 10:25:50 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+
+  const reset = () => setCount(0);
+
+  const double = () => {
+    setCount((previousCount) => previousCount * 2);
+  };
+
+  const devide = () => {
+    count % 3 === 0 ? setCount(previousCount => previousCount / 3) : setCount(previousCount => previousCount)
+  }
+
   return (
-    <div>
-      This is a template for React app.
-    </div>
+    <>
+      <div> count : {count} </div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+      </div>
+      <div>
+        <button onClick={double}>×2</button>
+      </div>
+      <div>
+        <button onClick={devide}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
